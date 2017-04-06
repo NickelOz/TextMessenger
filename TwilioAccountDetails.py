8eebc223c26b853a8e1d2f2cc0495c544f863fb2
Nicholas <NickelOz@users.noreply.github.com>
2017-04-06 17:17:46 +1000

    Uploaded TwilioAccountDetails.py
    A sample details file for a Twilio Account

---
+++ TwilioAccountDetails.py
@@ -0,0 +1,13 @@
+def SID():
+    accountSID = '' #Account SID
+    return accountSID
+
+def TOKEN():
+    authTOKEN = '' #Account Authentication Token
+    return authTOKEN
+
+def number():
+    return '' #Phone Number of Receiver
+
+def twilionumber():
+    return '' #Phone Number of Sender (Twilio Number)
